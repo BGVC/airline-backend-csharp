ee72a3ba9f25dce64e20da6cdd508d6cb9e3fdcf
borislav <grubovicborislav@gmail.com>
2019-03-06 02:56:11 +0100

    Initial code first setup.

---
+++ BGVC.Airline.Backend/BGVC.Airline.Backend/AirlineDBContext.cs
@@ -0,0 +1,25 @@
+﻿using BGVC.Airline.Backend.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BGVC.Airline.Backend
+{
+    public class AirlineDBContext : DbContext
+    {
+        public AirlineDBContext(DbContextOptions<AirlineDBContext> options) : base(options)
+        {
+            Database.Migrate();
+        }
+
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<City> Cities { get; set; }
+
+        internal void EnsureSeedDataForContext()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
