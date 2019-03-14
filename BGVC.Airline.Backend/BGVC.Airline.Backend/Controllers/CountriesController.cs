62c63011be6a5aea3f37ed9b561b768838b9101f
borislav <grubovicborislav@gmail.com>
2019-03-14 03:04:00 +0100

    #4 Countries Controller. Get routes

---
+++ BGVC.Airline.Backend/BGVC.Airline.Backend/Controllers/CountriesController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BGVC.Airline.Backend.Controllers
+{
+    [Route("api/[controller]")]
+    public class CountriesController : Controller
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var countries = Initialize.GetContext().Countries.Select(country =>
+                new
+                {
+                    country.Id,
+                    country.Name,
+                    country.Iso
+                }).ToList();
+
+            return Ok(countries);
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            var country = Initialize.GetContext().Countries.Single(x => x.Id == id);
+            var result = new
+            {
+                country.Id,
+                country.Name,
+                country.Iso
+            };
+
+            return Ok(result);
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
